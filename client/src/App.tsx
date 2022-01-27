ddc6ab0b17a45ddc6bbe514fb166a853bc547c1e
cse18107 <cse18107@cemk.ac.in>
2022-01-28 00:03:28 +0530

    working on some flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import Todo from './models/todo';
 import './App.css';
 import Todos from './components/Todos';
+
+const items :Todo[]=[new Todo(new Date().toString(),'Learn React'),new Todo(new Date().toString(),'Learn TypeScript')];
 
 function App() {
   return (
     <div>
-      <Todos/>
+      <Todos items={items}/>
     </div>
   );
 }
--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Todo from '../models/todo';
 
-function Todos() {
-  return <div>Todos</div>;
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+    return (
+        <ul>
+            {props.items.map((item) => {
+                return (<li key={item.id}>{item.text}</li>);
+            })}
+        </ul>
+    );
 }
 
 export default Todos;
